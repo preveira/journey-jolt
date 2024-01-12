c7eee777ef3796549b9cfb9233e92ec549c7fc9d
preveira <patrick.k.reveira@gmail.com>
2024-01-11 17:16:50 -0800

    added JS for prompts and ratings

--- app.js
+++ app.js
@@ -3,28 +3,36 @@ function sayHello () {
     document.write("<p>Hello " + user +"! </p>");
 }
 
-let favCountry = prompt('What is your favorite country?');
-function loveCountry () {
-document.write("<p>" + user + " favorite county: " + favCountry + "</p>");
-}
-
-let y_or_n = prompt("Looking for great travel prices?");
-if(y_or_n === "yes")
-function travelSites() {
-    document.write("<p>Alright, check out <a href=https://www.trivago.com/>these</a> great deals</p>");
-}else {
-    document.write("Okay well maybe next time! Have a great day...");
-}
-
-
-function review();
-    let rating;
-    let count = 0;
-    while(!rating || rating > 5){
-        rating = parseInt(prompt("rate us! 1 - 5"));
-    }
-
+let favCountry;
 do {
-    document.write('<img src="https://t3.ftcdn.net/jpg/00/94/22/12/360_F_94221253_fAgv0Jym53ysz7nwoqXRmkb6GAKwNsfs.jpg">');
-    count ++;
-} while (count < rating);
+    favCountry = prompt('What is your favorite country?');
+} while (!favCountry);
+
+function loveCountry() {
+    document.write("<p>" + user + " favorite country: " + favCountry + "</p>");
+}
+
+let userRating;
+do {
+    userRating = prompt("What star rating would you give, 1-5?");
+} while (userRating === null || isNaN(userRating) || userRating < 1 || userRating > 5);
+
+function showRating() {
+    alert("Thank you for rating us with " + userRating + " stars!");
+}
+
+function starRating() {
+for (let i = 0; i < userRating; i++) {
+    document.write("<img src='https://m.media-amazon.com/images/I/51Y4UIq7iML._AC_SY695_.jpg' alt='Gold star' />");
+    document.write("<p>Thank you for rating us!</p>")
+}
+}
+
+let lookingForTravelPrices = prompt("Looking for great travel prices?");
+if (lookingForTravelPrices.toLowerCase() === "yes") {
+    function travelSites() {
+        document.write("<p>Alright, check out <a href='https://www.trivago.com/'>these</a> great deals</p>");
+    }
+} else {
+    document.write("Okay, well maybe next time! Have a great day...");
+}
